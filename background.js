5f163bd5b6ccbee83466ab78016a294a584bbd7f
yukihane <yukihane.feather@gmail.com>
2024-12-21 09:41:58 +0900

    copilotの修正

--- background.js
+++ background.js
@@ -1,29 +1,36 @@
-chrome.contextMenus.create({
-  title: chrome.i18n.getMessage("menu_title"),
-  onclick: function(info, tab) {
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    title: chrome.i18n.getMessage("menu_title"),
+    id: "closeOtherTabs",
+    contexts: ["all"],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "closeOtherTabs") {
     const windowId = tab.windowId;
 
     closeTabs(windowId);
 
     // close other windows
-    chrome.windows.getAll({}, function(windows) {
-      const targets = windows.filter(w => w.id !== windowId);
-      for (t of targets) {
+    chrome.windows.getAll({}, function (windows) {
+      const targets = windows.filter((w) => w.id !== windowId);
+      for (const t of targets) {
         chrome.windows.remove(t.id);
       }
     });
   }
 });
 
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.action.onClicked.addListener(function (tab) {
   const windowId = tab.windowId;
   closeTabs(windowId);
 });
 
-// close same window tabs
+// close same window inactive tabs
 function closeTabs(windowId) {
-  chrome.tabs.query({ active: false, windowId: windowId }, function(tabs) {
-    const tabIds = tabs.map(t => t.id);
+  chrome.tabs.query({ active: false, windowId: windowId }, function (tabs) {
+    const tabIds = tabs.map((tab) => tab.id);
     chrome.tabs.remove(tabIds);
   });
 }
